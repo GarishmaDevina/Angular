d52b3508633f8e94c8661b991e90323994a5d3df
GarishmaDevina <garishma2002@gmail.com>
2024-03-21 19:19:44 +0530

    firstgit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'DemoApp';
+  
+  title:string= 'DemoApp';
+  city:string="Hyderabad";
+  n:string="App";
+  d:string=new Date().toLocaleTimeString();
+  timeid=setInterval(()=>{
+    this.d=new Date().toLocaleTimeString();
+  },1000)
+  changeName(e:any){
+     console.log(e);
+     console.log(e.target);
+     console.log(e.target.value);
+     
+     
+  }
 }
